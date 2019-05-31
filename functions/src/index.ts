b96ae1f4764e096e23095f8c23c464dfefb1e534
Vai van Ommeren <vaivanommeren@Eros.local>
2019-05-31 14:19:09 +0200

    modifications

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,37 +5,46 @@ import { DocumentData } from '@google-cloud/firestore';
 
 export const createNotification = functions.https.onRequest((req, res) => {
     
+    res.set('Access-Control-Allow-Origin', '*');
 
     if(req.method === 'POST'){
 
         const notifications: changeNotification[] = req.body
 
+        const promises: Promise<any>[] = []
         notifications.forEach(x => {
-            Firebase.firestore().collection('notifications').add(x).then(() =>{
-                console.log(`${x} + added to the database`)
+            promises.push(Firebase.firestore().collection('notifications').add(x))
+            Promise.all(promises).then(() =>{
+                res.status(201).send('items added to the database')
             }).catch((err) =>{
                 res.status(400).send(err)
             });
         });
 
-        res.status(201).send('items added to the database')
-    }else{
-        res.status(405).send("This HTTP Method is not allowed");
+       
+    }else if (req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', req.header('Access-Control-Request-Headers'));
+        res.header('Access-Control-Allow-Methods', req.header('Access-Control-Request-Method'));
+        res.status(204).send('');
     }
+
 });
 
 export const getNotifications = functions.https.onRequest((req, res) => {
+
+    res.set('Access-Control-Allow-Origin', '*');
     
     if(req.method === 'GET'){
         const uid = req.query.uid;
 
         if(uid !== null){
-            let notifications: DocumentData[] = []
+            const notifications: DocumentData[] = []
             Firebase.firestore().collection('notifications')
-            .where('userid', '==', uid).get().then((data) => {
-
+            .where('userid', '==', uid)
+            .get().then((data) => {
                 if(data.empty){
-                    res.status(400).send("no notifications for this user")
+                    res.status(204).send('')
                 }else{
                     data.forEach(doc => {
                         notifications.push(doc.data())
@@ -49,36 +58,51 @@ export const getNotifications = functions.https.onRequest((req, res) => {
         }else {
             res.status(400).send("a name is needed.")
         } 
-    }else{
-        res.status(405).send("This HTTP Method is not allowed");
+    }else if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', req.header('Access-Control-Request-Headers'));
+        res.header('Access-Control-Allow-Methods', req.header('Access-Control-Request-Method'));
+        res.status(204).send('');
     }
 })
 
 export const editNotification = functions.https.onRequest((req, res) => {
     
-    const notification: changeNotification = req.body
+    res.set('Access-Control-Allow-Origin', '*');
+
+
 
     if(req.method === 'PATCH'){
-        const nRef = Firebase.firestore().collection('notifications')
-        .where('userid', '==', notification.userid)
-        .where('jobofferid', '==', notification.jobofferid)
 
+        const notifications: changeNotification[] = req.body
 
-        Firebase.firestore().runTransaction(t => {
-            return t.get(nRef)
-            .then(data => {
-                data.forEach(doc => {
-                    t.update(doc.ref, {read: true})
+        const promises: Promise<any>[] = []
+
+        notifications.forEach((x) =>{
+             const nRef = Firebase.firestore().collection('notifications')
+            .where('userid', '==', x.userid)
+            .where('jobofferid', '==', x.jobofferid)
+
+            promises.push(Firebase.firestore().runTransaction(t => {
+                return t.get(nRef)
+                .then(data => {
+                    data.forEach(doc => {
+                        t.update(doc.ref, {read: true})
+                    })
                 })
-            })
-        }).then(result => {
+            }))
+        })
+        Promise.all(promises).then(() => {
             console.log('Transaction Success')
-            res.status(200).send('notification updated')
+            res.status(200).send('notifications updated')
         }).catch(err => {
             console.log(err)
             res.status(500).send('Something went wrong')
         })
-    }else{
-        res.status(405).send('This HTTP Method is not allowed')
+    }else if (req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', req.header('Access-Control-Request-Headers'));
+        res.header('Access-Control-Allow-Methods', req.header('Access-Control-Request-Method'));
+        res.status(204).send('');
     }
 })
--- functions/src/models/notification.ts
+++ functions/src/models/notification.ts
@@ -3,10 +3,12 @@ export class changeNotification{
     userid: string;
     read: boolean;
     jobofferid: string;
+    joboffername: string;
     
-    constructor(userid: string, read: boolean, jobofferid: string){
+    constructor(userid: string, jobofferid: string, read: boolean, joboffername: string){
         this.userid = userid;
-        this.read = read;
         this.jobofferid = jobofferid;
+        this.read = read;
+        this.joboffername = joboffername;
     }
 }
